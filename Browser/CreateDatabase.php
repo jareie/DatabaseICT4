f03bd32b1b98179c2976c493446f9d569eb43650
Joel Andreas Hallheim Reiersøl <jareie15@student.uia.no>
2019-11-28 15:48:40 +0100

    Added php scripts for creating database and tables

---
+++ Browser/CreateDatabase.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "joel";
+$password = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE Measles";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+?>
