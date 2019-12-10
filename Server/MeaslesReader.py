776d7deb394772f0e8aa3baf840059e24dd5f485
Joel Andreas Hallheim Reiersøl <jareie15@uia.no>
2019-12-10 13:15:18 +0100

    Fix of data loading

--- Server/MeaslesReader.py
+++ Server/MeaslesReader.py
@@ -1,5 +1,5 @@
 import csv
-csvfile = open('measles.csv')
+csvfile = open('dataset.csv')
 reader = csv.DictReader(csvfile)
 
 
@@ -27,7 +27,7 @@ for row in reader:
     PeriodEndYear = PeriodEnd[0]'''
     #print(PeriodEnd)
 
-    string = "'" + row["ConditionName"] + "', " + row["ConditionSNOMED"] + ", '" + row["PathogenName"] + "', '" + row["PathogenTaxonID"] + "', " + row["Fatalities"] + ", '" + row["CountryName"] + "', '" + row["CountryISO"] + "', '" + row["Admin1Name"] + "', '" + row["Admin1ISO"] + "', '" + row["Admin2Name"] + "', '" + row["CityName"] + "', '" + row["PeriodStartDate"] + "', '" + row["PeriodEndDate"] + "', " + row["PartOfCumulativeCountSeries"] + ", '" + row["AgeRange"] + "', '" + row["Subpopulation"] + "', '" + row["PlaceOfAcquisition"] + "', '" + row["DiagnosisCertainty"] + "', '" + row["SourceName"] + "', " + row["CountValue"] + ""
+    string = "'" + row["ConditionName"] + "', " + row["ConditionSNOMED"] + ", '" + row["PathogenName"] + "', '" + row["PathogenTaxonID"] + "', " + row["Fatalities"] + ", '" + row["CountryName"] + "', '" + row["CountryCode"] + "', '" + row["Admin1Name"] + "', '" + row["Admin1ISO"] + "', '" + row["Admin2Name"] + "', '" + row["CityName"] + "', '" + row["PeriodStartDate"] + "', '" + row["PeriodEndDate"] + "', " + row["PartOfCumulativeCountSeries"] + ", '" + row["AgeRange"] + "', '" + row["Subpopulation"] + "', '" + row["PlaceOfAcquisition"] + "', '" + row["DiagnosisCertainty"] + "', '" + row["SourceName"] + "', " + row["CountValue"] + ""
     #print(string)
     mycursor.execute('INSERT INTO data (ConditionName,ConditionSNOMED,PathogenName,PathogenTaxonID,Fatalities,CountryName,CountryISO,Admin1Name,Admin1ISO,Admin2Name,CityName,PeriodStartDate,PeriodEndDate,PartOfCumulativeCountSeries,AgeRange,Subpopulation,PlaceOfAcquisition,DiagnosisCertainty,SourceName,CountValue) VALUES(' + string + ');')
     
